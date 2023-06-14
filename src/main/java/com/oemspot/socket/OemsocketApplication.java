2e5a47ad4ccf5b5cd84552b706d973ae015a7edb
Alexis Georgopulos [Windows] <alexgeo683@gmail.com>
2023-06-14 00:46:37 -0300

    Primera version

---
+++ src/main/java/com/oemspot/socket/OemsocketApplication.java
@@ -0,0 +1,13 @@
+package com.oemspot.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OemsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oemspot/socket/OemsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oemspot.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OemsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
